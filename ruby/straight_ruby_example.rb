05450962fa1ee4e6213ba51d1ba02010b0c9ec25
Michael Kuehl <michael@expectedbehavior.com>
2010-10-25 11:56:35 -0400

    switch straight ruby to use the gem

--- ruby/straight_ruby_example.rb
+++ ruby/straight_ruby_example.rb
@@ -1,31 +1,20 @@
-# Once you put your API key in below, you can run this using
-# ruby straight_ruby_example.rb
-# and two files docraptor_sample.pdf and docraptor_sample.xls will be created in the current directory
-class Doc
-  API_KEY = "YOUR_API_KEY_HERE"
+require 'rubygems'
+require 'doc_raptor'
 
-  require 'rubygems'
-  require 'httparty'
-  include HTTParty
-  base_uri "https://docraptor.com"
-  
-  # :document_content => content, :name => "a doc", :document_type => "xls", :test => false
-  # returns a string that is the document data
-  def self.create(document_information)
-    post("/docs", :body => {:doc => document_information}, :basic_auth => {:username => API_KEY})
-  end
-end
+DocRaptor.api_key "YOUR_API_KEY_HERE"
 
+xls_html = "<table name='My First Sheet'><tr><td>Cell 1</td><td>Cell 2</td></tr></table>"
 File.open("docraptor_sample.xls", "w+") do |f| 
-  f.write Doc.create(:document_content => "<table name='My First Sheet'><tr><td>Cell 1</td><td>Cell 2</td></tr></table>",
-                     :name             => "docraptor_sample.xls",
-                     :document_type    => "xls",
-                     :test             => true)
+  f.write DocRaptor.create(:document_content => xls_html,
+                           :name             => "docraptor_sample.xls",
+                           :document_type    => "xls",
+                           :test             => true)
 end
 
+pdf_html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html lang="en"><body><a href="http://google.com">google</a></body></html>'
 File.open("docraptor_sample.pdf", "w+") do |f| 
-  f.write Doc.create(:document_content => "<html><body>Text in a PDF</body></html>",
-                     :name             => "docraptor_sample.pdf",
-                     :document_type    => "pdf",
-                     :test             => true)
+  f.write DocRaptor.create(:document_content => pdf_html,
+                           :name             => "docraptor_sample.pdf",
+                           :document_type    => "pdf",
+                           :test             => true)
 end
